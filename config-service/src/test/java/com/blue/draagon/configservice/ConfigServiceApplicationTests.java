d2f6b6809ec4f94153a50c4572753a6a873e7349
Khalifa Sene <khalifaasene10@gmail.com>
2024-12-14 13:53:57 +0000

    spring cloud base app discovery config repositoty

---
+++ config-service/src/test/java/com/blue/draagon/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.draagon.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/blue/draagon/Main.java
@@ -0,0 +1,7 @@
+package com.blue.draagon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Blue Draagon");
+    }
+}
